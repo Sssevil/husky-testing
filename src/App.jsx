05e5ac7e921dc9ff408f6b02e234e76b2b5171ca
Sssevil <zhunushova29@gmail.com>
2024-06-26 00:50:24 +0600

    'ggfghdg'

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
 import { useState } from "react";
 import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg"
+import viteLogo from "/vite.svg";
 import "./App.css";
 
 const App = () => {
-  const [ count, setCount ] = useState(0)
+  const [ count, setCount ] = useState(0);
 
   return (
     <>
